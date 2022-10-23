fad9ea361a2036a19bf48cccd114f10dfb15f580
Nikita Ezhov <nikita.ejov2012@yandex.ru>
2022-10-23 08:11:10 +0300

    backend some requests made

--- backend/main.py
+++ backend/main.py
@@ -62,14 +62,15 @@ exampleTimeDict = {
 
 
 def createRoute(elem):
-    return { 'startTime': -1,
-             'stopTime': -1,
-             'buses': elem['buses'],
-             'passengerCount': elem['passengers']
-             }
+    return {'startTime': -1,
+            'stopTime': -1,
+            'buses': elem['buses'],
+            'passengerCount': elem['passengers'],
+            'routeId': elem['routeId']
+            }
 
 
-def transformAlgorithmData(timeDict, startMinute=0):
+def transformAlgorithmData(timeDict):
     routes = {}
     for key, arr in timeDict.items():
         for elem in arr:
@@ -78,6 +79,11 @@ def transformAlgorithmData(timeDict, startMinute=0):
                 routes[elem['routeId']]['startTime'] = key
             else:
                 routes[elem['routeId']]['stopTime'] = key
+                routes[elem['routeId']]['id'] = key
+    return routes
+
+
+def transformSchedule(routes, startMinute=0):
     res = {}
     for key, elem in routes.items():
         if elem['stopTime'] > startMinute:
@@ -85,27 +91,47 @@ def transformAlgorithmData(timeDict, startMinute=0):
     return res
 
 
-@app.route('/about')
-def about():
-    return "About page"
+def transformSchedule(routes, startMinute=0):
+    res = {}
+    for key, elem in routes.items():
+        if elem['stopTime'] > startMinute:
+            res[key] = elem
+    return res
 
 
-@app.route('/routes/new')
-def des():
-    routeId = request.args.get('routeId')
-    startTime = request.args.get('startTime')
-    busNumbers = request.args.get('busNumbers')
-    print("/routes/new", routeId, startTime, busNumbers)
-
-    return jsonify(
-        username="das",
-        email="fsd",
-        id="sdfd"
-    )
+schedule = transformAlgorithmData(exampleTimeDict)
 
 
-@app.route('/routes/update')
-def des2():
+@app.errorhandler(404)
+def handel_404(error):
+    return Response(status=404)
+
+
+@app.errorhandler(400)
+def handel_400(error):
+    return Response(status=400)
+
+
+@app.route('/routes/new', methods=['POST'])
+def routeNew():
+    routeId = int(request.args.get('routeId'))
+    startTime = int(request.args.get('startTime'))
+    stopTime = int(request.args.get('stopTime'))
+    startPoint = int(request.args.get('startPoint'))
+    stopPoint = int(request.args.get('stopPoint'))
+    passengers = int(request.args.get('passengers'))
+    buses = request.args.get('buses')
+    buses = [int(val) for val in buses.split(',')]
+    schedule[routeId] = {}
+    schedule[routeId].update({'routeId': routeId, 'startTime': startTime, 'stopTime': stopTime,
+                              'startPoint': startPoint, 'stopPoint': stopPoint, 'passengerCount': passengers,
+                              'buses': buses})
+
+    return Response(status=200)
+
+
+@app.route('/routes/update', methods=['POST'])
+def routeUpdate():
     routeId = request.args.get('routeId')
     startTime = request.args.get('startTime')
     print("/routes/update", routeId, startTime)
@@ -117,9 +143,48 @@ def des2():
     )
 
 
-@app.route('/day')
-def des3():
-    return Response(status=400, mimetype='application/json')
+@app.route('/routes/deleteBus', methods=['DELETE'])
+def routeDeleteBus():
+    routeId = request.args.get('routeId')
+    startTime = request.args.get('startTime')
+    print("/routes/update", routeId, startTime)
+
+    return jsonify(
+        username="das",
+        email="fsd",
+        id="sdfd"
+    )
+
+
+@app.route('/routes/addBus', methods=['UPDATE'])
+def routeAddBus():
+    routeId = request.args.get('routeId')
+    startTime = request.args.get('startTime')
+    print("/routes/update", routeId, startTime)
+
+    return jsonify(
+        username="das",
+        email="fsd",
+        id="sdfd"
+    )
+
+
+@app.route('/day', methods=['GET'])
+def getDaySchedule():
+    global schedule
+    return jsonify(
+        list(schedule.values())
+    )
+
+
+@app.route('/fromMinute', methods=['GET'])
+def getCurrentSchedule():
+    global schedule
+    startTime = request.args.get('minute')
+    schedule = transformSchedule(schedule, startMinute=int(startTime))
+    return jsonify(
+        list(schedule.values())
+    )
 
 
 if __name__ == "__main__":

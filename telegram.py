9e430933015d16dc02c44e99990f68433162f6ee
Masloyka <115878542+Masloyka@users.noreply.github.com>
2023-01-30 18:54:44 +0300

    Add files via upload

---
+++ telegram.py
@@ -0,0 +1,34 @@
+from aiogram import Dispatcher, Bot, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+
+API = '6074913120:AAFRQ-3MYhA70VWvDaLXH2hxYh2L0PWstwI'
+
+bot = Bot(API)
+update = Dispatcher(bot)
+
+
+button_hi = KeyboardButton('Я максимально готов сдать ЕГЭ')
+kb1 = ReplyKeyboardMarkup()
+kb1.add(button_hi)
+kb1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hi)
+
+@update.message_handler(commands=['start'])
+async def button_hi(message: types.message):
+    await message.reply('Здравствуйте,флешка!\nЭтот бот поможет сдать ЕГЭ на 100!!!\nЖду твоего сообщения', reply_markup=kb1)
+
+
+
+@update.message_handler(commands=['start'])
+async def welcome(message: types.message):
+    await message.reply("Здравствуйте,флешка!\nЭтот бот поможет сдать ЕГЭ на 100!!!\nЖду твоего сообщения")
+
+@update.message_handler(commands=['help'])
+async def help(message: types.message):
+    await  message.reply('Напиши мне что-нибудь и получишь ответ')
+
+@update.message_handler()
+async def answer(message: types.message):
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(update, skip_updates=True)
